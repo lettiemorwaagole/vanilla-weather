cba37df6ad63ed9bacad0f8ad9c08b97a3bebdee
lettie <lettiekmorwaagole@gmail.com>
2023-08-12 21:18:15 +0200

    added the weather icon

--- src/app.js
+++ src/app.js
@@ -1,26 +1,4 @@
-function formatDate(timestamp) {
-    let date = new Date(timestamp);
-    let hours = date.getHours();
-    if (hours < 10) {
-      hours = `0${hours}`;
-    }
-    let minutes = date.getMinutes();
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
-  
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-    let day = days[date.getDay()];
-    return `${day} ${hours}:${minutes}`;
-  }
+
 
 function displayTemperature(response) {
     console.log(response.data);
@@ -34,12 +12,39 @@ function displayTemperature(response) {
     humidityElement.innerHTML=response.data.temperature.humidity;
     let windElement=document.querySelector("#wind");
     windElement.innerHTML=Math.round(response.data.wind.speed);
-    let dateElement = document.querySelector("#date")
+    let dateElement = document.querySelector("#date");
     dateElement.innerHTML = formatDate(response.data.time * 1000);
+    let iconElement=document.querySelector("#icon");
+    iconElement.setAttribute("src",'https://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png');
+
+    
 
 }
 
 let apiKey = "f2a194ebftba7cf780c1718655ff3do3";
 let apiUrl ='https://api.shecodes.io/weather/v1/current?query=Gaborone&key=f2a194ebftba7cf780c1718655ff3do3&units=metric';
 
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 axios.get(apiUrl).then(displayTemperature);
