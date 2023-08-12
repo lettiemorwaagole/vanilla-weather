45bbfc5c7bf46b0af2ca70d571924dc80b1e1935
lettie <lettiekmorwaagole@gmail.com>
2023-08-12 21:49:19 +0200

    adding a search box

--- src/app.js
+++ src/app.js
@@ -1,28 +1,27 @@
 
 
 function displayTemperature(response) {
-    console.log(response.data);
-    let temperatureElement=document.querySelector("#temperatureid");
-    temperatureElement.innerHTML=Math.round(response.data.temperature.current);
-    let cityElement=document.querySelector("#city");
-    cityElement.innerHTML=response.data.city;
-    let descriptionElement=document.querySelector("#description");
-    descriptionElement.innerHTML=response.data.condition.description;
-    let humidityElement=document.querySelector("#humidity");
-    humidityElement.innerHTML=response.data.temperature.humidity;
-    let windElement=document.querySelector("#wind");
-    windElement.innerHTML=Math.round(response.data.wind.speed);
-    let dateElement = document.querySelector("#date");
-    dateElement.innerHTML = formatDate(response.data.time * 1000);
-    let iconElement=document.querySelector("#icon");
-    iconElement.setAttribute("src",'https://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png');
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperatureid");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute("src", 'https://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png');
 
-    
+
 
 }
 
-let apiKey = "f2a194ebftba7cf780c1718655ff3do3";
-let apiUrl ='https://api.shecodes.io/weather/v1/current?query=Gaborone&key=f2a194ebftba7cf780c1718655ff3do3&units=metric';
+
 
 function formatDate(timestamp) {
   let date = new Date(timestamp);
@@ -47,4 +46,19 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
-axios.get(apiUrl).then(displayTemperature);
+
+function search(city) {
+  let apiKey = "f2a194ebftba7cf780c1718655ff3do3";
+  let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=Gaborone&key=f2a194ebftba7cf780c1718655ff3do3&units=metric';
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Gaborone");
+
+let form = document.querySelector("search-form");
+form.addEventListener("submit", handleSubmit);
